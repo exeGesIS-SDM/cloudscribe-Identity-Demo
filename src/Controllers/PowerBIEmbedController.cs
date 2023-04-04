4efcfdbaf2bd16926c1dcb4ce708661c73a31d5d
Jim Kerslake <39943820+JimKerslake@users.noreply.github.com>
2023-04-04 14:47:35 +0100

    Install the necessary bits

---
+++ src/Controllers/PowerBIEmbedController.cs
@@ -0,0 +1,23 @@
+﻿using cloudscribe_identity_demo.EmbedWeb;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Authorization;
+
+namespace cloudscribe_identity_demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PowerBIEmbedController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<EmbedInfo> Get()
+        {
+            Guid workspaceId = Guid.Parse("81f02fe5-c972-4a69-ab29-b13fbf0a12e3");  // officer >> open cases ?
+            Guid reportId = Guid.Parse("dcdca343-2314-433e-803c-276de8eb661f");
+
+            var embedInfo = await Embedder.GetEmbedInfo(workspaceId, reportId);
+            return embedInfo;
+        }
+    }
+}
---
+++ src/EmbedWeb/Embedder.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.PowerBI.Api;
+using Microsoft.PowerBI.Api.Models;
+using Microsoft.Rest;
+using System;
+using System.Threading.Tasks;
+
+namespace cloudscribe_identity_demo.EmbedWeb
+{
+    public class Embedder
+    {
+
+        private static readonly string _clientId = "SERVICE-PRINCIPAL-CLIENT-ID";
+        private static readonly string _clientSecret = "SERVICE-PRINCIPAL-CLIENT-SECRET";
+        private static readonly string _tenantId = "SERVICE-PRINCIPAL-TENANT-ID";
+
+        // Power BI Service API Root URL
+        const string urlPowerBiRestApiRoot = "https://api.powerbi.com/";
+
+        private static string GetAppOnlyAccessToken()
+        {
+            var tenantAuthority = $"https://login.microsoftonline.com/{_tenantId}";
+
+            var appConfidential = ConfidentialClientApplicationBuilder
+                .Create(_clientId)
+                .WithClientSecret(_clientSecret)
+                .WithAuthority(tenantAuthority)
+                .Build();
+
+            var scopesDefault = new string[] { "https://analysis.windows.net/powerbi/api/.default" };
+            var authResult = appConfidential
+                .AcquireTokenForClient(scopesDefault)
+                .ExecuteAsync()
+                .Result;
+
+            return authResult.AccessToken;
+        }
+
+        private static PowerBIClient GetPowerBiClient()
+        {
+            var tokenCredentials = new TokenCredentials(GetAppOnlyAccessToken(), "Bearer");
+            return new PowerBIClient(new Uri(urlPowerBiRestApiRoot), tokenCredentials);
+        }
+
+        public static async Task<EmbedInfo> GetEmbedInfo(Guid workspaceId, Guid reportId)
+        {
+            var pbiClient = GetPowerBiClient();
+
+            var report = await pbiClient.Reports.GetReportInGroupAsync(workspaceId, reportId);
+            var embedUrl = report.EmbedUrl;
+
+            var generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "View");
+
+            var tokenResponse = await pbiClient.Reports
+                .GenerateTokenInGroupAsync(workspaceId, reportId, generateTokenRequestParameters);
+
+            var embedToken = tokenResponse.Token;
+
+            return new EmbedInfo
+            {
+                ReportId = report.Id,
+                EmbedUrl = embedUrl,
+                AccessToken = embedToken
+            };
+
+        }
+    }
+
+    public class EmbedInfo
+    {
+        public Guid ReportId { get; set; }
+        public string EmbedUrl { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
